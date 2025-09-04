28da589689e00b63e5b0000fd6974d0ff47ba876
Mayank Goyal <mayankgoyal587>
2025-09-05 01:48:40 +0530

    Meals

---
+++ src/components/Meals.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import axios from "axios"
+import "../style2.css";
+import { useState ,useEffect } from 'react';
+
+
+const Meals = () => {
+  const [items,setItems]=  useState([]) 
+
+  useEffect(()=>{
+    axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+    .then((res)=>{
+        setItems(res.data.meals)
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+  },[])
+
+  const itemsList=items.map(({strMeal,strMealThumb,idMeal})=>{
+    return(
+        <section className='card'>
+            <img src={strMealThumb} alt={strMeal}/>
+            <section className='content'>
+                <p>{strMeal}</p>
+                <p>#{idMeal}</p>
+            </section>
+        </section>
+    )
+  })
+  return (
+    <div className='items-container'>{itemsList}</div>
+  )
+}
+
+export default Meals
